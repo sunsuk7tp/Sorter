5b4992d44dd16434dcf7df34aa4b80e48668f5bd
Shunsuke Nakamura <sunsuk7tp@gmail.com>
2010-07-17 07:58:48 +0900

    Imply simple qsort using DFS and pass the test(t/sort.t)

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,53 @@
-# implement this
+package Sorter;
+
+use strict;
+use warnings;
+use base 'Class::Accessor';
+
+sub values {
+    my $self = shift;
+    if(@_) {
+	$self->{values} = $_[0];
+    }
+    $self->{values};
+}
+
+sub new {
+    my $self = shift;
+    $self->SUPER::new({values => [], @_});
+}
+
+sub set_values {
+    my $self = shift;
+    $self->values([@_]);
+}
+
+sub get_values {
+    my $self = shift;
+    @{$self->values};
+}
+
+sub sort {
+    my $self = shift;
+    my @values = $self->get_values;
+    if(scalar @values <= 1) {
+	$self->get_values;
+    } else {
+	my $root = shift @values;
+	
+	my @lvals, my @rvals;
+	foreach (@values) {
+	    ($_ < $root) ? push(@lvals, $_) : push(@rvals, $_); 
+        }
+	my $lleaf = __PACKAGE__->new;
+        my $rleaf = __PACKAGE__->new;
+	$lleaf->set_values(@lvals);
+	$rleaf->set_values(@rvals);
+	$lleaf->sort;
+	$rleaf->sort;
+	
+	$self->set_values($lleaf->get_values, $root, $rleaf->get_values);
+    }
+}
+
+1;
