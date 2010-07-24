a70e34399c162cc94bf02dd7f4e8e244510eea34
Shunsuke Nakamura <sunsuk7tp@gmail.com>
2010-07-25 01:47:37 +0900

    use grep.

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -6,41 +6,37 @@ use base 'Class::Accessor';
 Sorter->mk_accessors(qw(values));
 
 sub new {
-	my $self = shift;
-	$self->SUPER::new({values => [], @_});
+    my $self = shift;
+    $self->SUPER::new({values=>[], @_});
 }
 
 sub set_values {
-	my $self = shift;
-	$self->values([@_]);
+    my $self = shift;
+    $self->values([@_]);
 }
 
 sub get_values {
-	my $self = shift;
-	@{$self->values};
+    my $self = shift;
+    @{$self->values};
 }
 
 sub sort {
-	my $self = shift;
-	my @values = $self->get_values;
-	if(scalar @values <= 1) {
-		$self->get_values;
-	} else {
-		my $root = shift @values;
+    my $self = shift;
+    my @values = $self->get_values;
+    if(scalar @values <= 1) {
+	$self->get_values;
+    } else {
+	my $root = shift @values;
+
+	my $lleaf = __PACKAGE__->new;
+	my $rleaf = __PACKAGE__->new;
+	$lleaf->set_values(grep {$_ < $root} @values);
+	$rleaf->set_values(grep {$_ >= $root} @values);
+	$lleaf->sort;
+	$rleaf->sort;
 		
-		my @lvals, my @rvals;
-		foreach (@values) {
-			($_ < $root) ? push(@lvals, $_) : push(@rvals, $_);
-		}
-		my $lleaf = __PACKAGE__->new;
-		my $rleaf = __PACKAGE__->new;
-		$lleaf->set_values(@lvals);
-		$rleaf->set_values(@rvals);
-		$lleaf->sort;
-		$rleaf->sort;
-		
-		$self->set_values($lleaf->get_values, $root, $rleaf->get_values);
-	}
+	$self->set_values($lleaf->get_values, $root, $rleaf->get_values);
+    }
 }
 
 1;
